b2ae4d1d3e0f146534123d217454ca9bd5b4e192
Jonny Muir <jonnypmuir@gmail.com>
2024-02-28 20:24:09 +0000

    Initial commit of Azure Function projects

---
+++ CalculationTools/RunExcelCalc.cs
@@ -0,0 +1,76 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using ExcelService;
+
+namespace CalculationTools
+{
+    public class RunExcelCalc
+    {
+        private readonly ILogger _logger;
+
+        public RunExcelCalc(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<RunExcelCalc>();
+        }
+
+        [Function("RunExcelCalc")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function called.");
+
+            // Parse JSON input
+            var jsonString = new StreamReader(req.Body).ReadToEnd();
+            var inputData = JsonConvert.DeserializeObject<dynamic>(jsonString);
+
+            // Validate input data (replace with your validation logic)
+            if (inputData == null || !inputData?.ContainsKey("spreadsheet") || !inputData?.ContainsKey("inputs") || !inputData?.ContainsKey("inputSheetName"))
+            {
+                var response = req.CreateResponse(HttpStatusCode.BadGateway);
+                response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+                response.WriteString("Invalid input data format");
+                return response;
+            }
+
+            // Process the data
+            try
+            {
+                // Decode base64-encoded spreadsheet
+                string spreadsheetString = inputData!.spreadsheet;
+
+                // Retrieve the target worksheet name
+                string targetWorksheetName = inputData.inputSheetName;
+
+                using Excel excel = new(spreadsheetString, targetWorksheetName);
+
+                // Input mapping
+                foreach (var item in inputData.inputs)
+                {
+                    string cellName = item.CellName;
+                    string cellValue = item.Value;
+                    excel.SetInput(cellName, cellValue);
+                }
+
+                var response = req.CreateResponse(HttpStatusCode.BadGateway);
+                response.Headers.Add("Content-Type", "application/octet-stream");
+                response.WriteBytes(excel.Save());
+                return response;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred processing the Excel file.");
+                var response = req.CreateResponse(HttpStatusCode.BadGateway);
+                response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+                response.WriteString("Error processing data");
+                return response;
+            }
+        } 
+    }
+}
---
+++ ExcelService/Excel.cs
@@ -0,0 +1,67 @@
+﻿namespace ExcelService;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+
+public class Excel : IDisposable
+{
+    private SpreadsheetDocument? spreadsheetDocument = null;
+    private Sheet? worksheet = null;
+    private MemoryStream? stream = null;
+    
+    public Excel(string spreadsheetString, string targetWorksheetName)
+    {
+        byte[] spreadsheetBytes = Convert.FromBase64String(spreadsheetString);
+        stream = new MemoryStream(spreadsheetBytes);
+
+        spreadsheetDocument = SpreadsheetDocument.Open(stream, true);
+
+        // Access the worksheet using the name
+        worksheet = spreadsheetDocument?.WorkbookPart?.Workbook.Descendants<Sheet>()
+                        .FirstOrDefault(s => s.Name == targetWorksheetName);
+
+        if (worksheet == null)
+        {
+            // Handle the case where the worksheet is not found
+            throw new Exception($"Sheet '{targetWorksheetName}' not found in the spreadsheet.");
+        }
+
+    }
+
+    public void Dispose()
+    {
+        if(spreadsheetDocument != null) 
+        {
+            spreadsheetDocument.Dispose();
+        }
+
+        if(stream != null)
+        {
+            stream.Dispose();
+        }
+    }
+
+    public void SetInput(string cellName, string cellValue)
+    {
+           // Find the cell by its address using the cell name
+        var cell = worksheet.Descendants<Cell>().FirstOrDefault(c => c.CellReference == cellName);
+
+        if (cell != null)
+        {
+            // Update the cell value
+            cell.CellValue = new CellValue(cellValue);
+        }
+        else
+        {
+            throw new Exception($"Cell '{cellName}' not found in input sheet'.");
+        }
+    }
+
+    public byte[] Save()
+    {
+        // Save the modified spreadsheet 
+        using var memoryStream = new MemoryStream();
+        spreadsheetDocument!.WorkbookPart!.Workbook.Save(memoryStream);
+        return memoryStream.ToArray();                 
+    }
+}
